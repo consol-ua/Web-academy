1755528b3f41b6f60798818bba0b91a7b8526efa
Malyhin Ivan <malygin91@gmail.com>
2020-11-16 19:17:57 +0200

    practic

---
+++ practic/#src/js/script.js
@@ -0,0 +1,39 @@
+//подключаем файл
+//@@include("alert.js");
+
+// поддержка webp?
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  }
+});
+
+function burgerClick() {
+  const burgerEl = document.querySelector('.header__burger');
+  const body = document.querySelector('body');
+  const menu = document.querySelector('.header__wrapper');
+  function addClass(e) {
+    if (e.target.className.includes('active')) {
+      burgerEl.className = 'header__burger'
+      body.className = '';
+      menu.className = 'header__wrapper'
+    } else {
+      burgerEl.className = 'header__burger active'
+      body.className = 'lock';
+      menu.className = 'header__wrapper active'
+    }
+  };
+  burgerEl.addEventListener("click", addClass);
+}
+
+
+burgerClick();
